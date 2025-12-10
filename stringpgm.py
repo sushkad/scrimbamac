50681762e6c808df37b9fcc8ffcdb4c415138698
sushantkadam <sushant.kadam27@gmail.com>
2025-12-11 00:15:41 +0530

    Stringpgm and Slicing

---
+++ stringpgm.py
@@ -0,0 +1,13 @@
+
+msg = "Welcome to Python 101: Strings"
+
+msg1 =msg[18]+''+msg[:8]+msg[25:29]+msg[7:11]+msg[13]+msg[12]+msg[2]+msg[1]+msg[-5]
+print(msg1.title())
+print(msg1.upper())
+print(msg1[::-1].title())
+
+
+QA = 'sushant'
+print(QA[-1])
+print(QA[:-2])
+print(QA[-2:])
---
+++ stringsbasics.py
@@ -0,0 +1,21 @@
+msg ='SuSHant'
+print(msg,msg)
+
+print(msg.upper())
+print(msg.lower())
+print(msg.title())
+print(msg.capitalize())
+
+print(len(msg))
+print(msg.count('S'))
+
+
+# slicing string
+
+print(msg[-3])
+print(msg[-5])
+print(msg[2:7])
+
+
+
+
