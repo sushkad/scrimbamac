1ccc81cf6ccacfd2245ecdbb98c81efde7763a3d
sushantkadam <sushant.kadam27@gmail.com>
2025-12-18 22:30:54 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,11 @@
+
+# calling , parameters , arguments , default
+
+# def means define
+
+
+def greeting(name, age):
+    print("Hello " + name + "! " + age )
+
+name = input("Enter your name: ")
+greeting("Sush" , "35")
