0d78f5141e6b63fda9afb9d8d6b46a02d56ca3e6
sushantkadam <sushant.kadam27@gmail.com>
2025-12-29 23:22:06 +0530

    Setpgm

--- phonenumbernormalizerchallenger.py
+++ phonenumbernormalizerchallenger.py
@@ -7,3 +7,22 @@
 # 5. Check if the cleaned number has **exactly 10 digits**.
 # 6. If yes, format it like this: (123) 456-7890
 # 7. If not, print an error message: "Please enter exactly 10 digits."
+
+
+
+friends = {'sushant','sid','nitin','sush','John'}
+my_friends = {'Reg','Colin','John',}
+
+cars = ['900','420','V70','996']
+
+print('sid' in friends and 'John' in friends)
+print(friends.intersection(my_friends))
+
+print(my_friends.symmetric_difference(friends))
+
+# print(friends.union(my_friends))
+print(friends & my_friends)
+
+
+cars_no_dupl = list(set(cars))
+print(cars_no_dupl)
