baa528d66b107b18b3695de530ac6f14d68e719b
sushantkadam <sushant.kadam27@gmail.com>
2025-12-30 21:49:19 +0530

    sort and sorted

---
+++ sortandsorted.py
@@ -0,0 +1,18 @@
+
+my_list = [15,5,3,8,7,9,11,18,15]
+
+my_dict ={'car':4,'dog':5,'add':6,'bee':11}
+
+my_tuple = ('d','c','a','z','n')
+
+my_string = 'python'
+
+
+print(my_dict, 'original')
+#print(my_list.sort())
+#print(my_list.reverse())
+print(sorted(my_dict.values(),reverse=True))
+print(my_dict,'new')
+
+
+print(my_list[:-1])
---
+++ sortedpgm.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+my_list = [1,5,-3,7,-2]
+my_llist=[['car',4,65],['dog',2,30],['add',3,10],['bee',1,24]]
+
+
+print(sorted(my_llist,key= lambda item: item[2]))
+
+
