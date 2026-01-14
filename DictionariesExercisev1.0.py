314f6f68fc04e035c50df8f412e8483fc17432e6
sushantkadam <sushant.kadam27@gmail.com>
2026-01-14 23:46:44 +0530

    DictionariesExcercise v1.0

--- DictionariesExercisev1.0.py
+++ DictionariesExercisev1.0.py
@@ -14,19 +14,13 @@ freelancers = {'name':'freelancing Shop','brian': 70, 'black knight':20, 'biccus
 antiques = {'name':'Antique Shop','french castle':400, 'wooden grail':3, 'scythe':150, 'catapult':75, 'german joke':5}
 pet_shop = {'name':'Pet Shop','blue parrot':10, 'white rabbit':5, 'newt': 2}
 
-
-
-
-
-
-
-
 #create an dempty shopping cart
 cart = {}
 #loop through stores/dicts
-for LOOP OVER THE SHOPS :
+for shop in (freelancers,antiques,pet_shop) :
     #inputbox  to show what you can buy...capture textstring of what was bought...make lowercase
-    buy_item = input(f'Welcome to {SHOPNAME}! what do you want to buy: {LIST ITEMS FOR SALE})
+    buy_item = input(f'Welcome to {shop["name"]}! what do you want to buy: {shop}').lower()
     #update the cart
-    cart.update({insert KEYVAL:VALUE}) # use pop...
-print(f'You Purchased {ITEMS PUCHASED} Today it is all free. Have a nice day of mayhem!')
+    cart.update({buy_item:shop.pop(buy_item)}) # use pop...
+    buy_items = ", ".join(list(cart.keys()))
+print(f'You Purchased {buy_items}. Today it is all free. Have a nice day of mayhem!')
