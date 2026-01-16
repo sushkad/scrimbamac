409b335dc1b1f31d7175cb64531f8f8d1a7cbae8
sushantkadam <sushant.kadam27@gmail.com>
2026-01-16 23:22:35 +0530

    exceptions

---
+++ Exceptions-tryexceptraise.py
@@ -0,0 +1,14 @@
+
+try:
+
+    num = int(input("Enter a number: "))
+    print("30 divided by",num, "is: ",30/num)
+
+except ZeroDivisionError as err:
+    print("You can't divide by zero")
+
+except ValueError as err:
+    print("You can't divide by zero")
+
+finally:
+    print("** Thank you for playing!**")
