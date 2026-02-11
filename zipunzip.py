03c3d99cd110ae801bc6afbe89688d2d3d6c1b3d
sushantkadam <sushant.kadam27@gmail.com>
2026-02-11 23:19:10 +0530

    zipnunzip update

--- zipunzip.py
+++ zipunzip.py
@@ -1,9 +1,16 @@
 
-nums =[1,2,3,4]
+nums = '1234'
 letters =['a','b','c','d','e']
 names =['John','Eric','Michael','Graham','Joe']
 
 combo = list(zip(nums,letters,names))
 print(combo)
+
+
+num , let , name = zip(*combo)
+
+print(num , let, name)
+
+
 
 
