51fabe227957e6421fcefc289e6c378093a379c7
sushantkadam <sushant.kadam27@gmail.com>
2026-02-25 20:08:34 +0530

    Interview Qns

---
+++ Interview.py
@@ -0,0 +1,22 @@
+
+
+student = {"name":"sushant","age":"35","role":"QA"}
+
+print(student["name"])
+
+print(student["age"])
+
+student["age"]="35"
+
+
+print(student)
+
+
+
+coordinates = (10,20)
+print(coordinates[1])
+
+def get_user():
+    return("QA",35)
+
+role, age = get_user()
