5257a08369644ce44c8eea8dd1c5ea02e1f76600
sushantkadam <sushant.kadam27@gmail.com>
2026-03-01 23:55:26 +0530

    String27

---
+++ string27.py
@@ -0,0 +1,14 @@
+
+
+my_list = [1,2,3,4,5]
+my_string = "sushant"
+
+
+slice_list = my_list.copy()
+
+slice_list1 = my_list[2:4]
+slice_string = my_list[1:4]
+
+print(slice_list)
+print(slice_list1)
+print(slice_string)
