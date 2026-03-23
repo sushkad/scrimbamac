717e2189e95750264e7b559250cdb85b2311ebb6
sushantkadam <sushant.kadam27@gmail.com>
2026-03-23 23:34:51 +0530

    Functions (Reusable Logic)

---
+++ Functions (Reusable Logic).py
@@ -0,0 +1,13 @@
+
+def add (a,b):
+
+    return a + b
+
+
+result  = add (10,20)
+
+print("Sum: ", result)
+
+print("Subtraction: ", result)
+print("Multiplication: ", result)
+print("Division: ", result)
