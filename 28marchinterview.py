f1f9a695f966865f913320d9b8c0bc83617a4020
sushantkadam <sushant.kadam27@gmail.com>
2026-03-28 16:26:46 +0530

    28marchinterview

---
+++ 28marchinterview.py
@@ -0,0 +1,27 @@
+
+
+x = {1,2,3}
+
+print(len(x))
+
+
+y =[1,2,3]
+print(y[:2])
+
+
+
+for i in range(1,5):
+    print(i,end="" "")
+
+print('''''''''''''')
+
+
+a = [1,2]
+a.append(3)
+print(a)
+
+'''
+print("Python"[1])
+print(a)
+'''
+
