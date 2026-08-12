10ee0948e643e941dbc00b1a7ae72a735ce1daf8
sushantkadam <sushant.kadam27@gmail.com>
2026-08-12 23:19:13 +0530

    typecheck and conversion

---
+++ type check and coversion.py
@@ -0,0 +1,13 @@
+
+
+hourly_rate = float(input("Enter your hourly rate ?"))
+hourly_work = int(input("Enter your hourly worked ?"))
+
+print(type(hourly_rate))
+print(type(hourly_work))
+
+
+total_pay = hourly_work * hourly_rate
+
+print(type(total_pay))
+print(total_pay)
