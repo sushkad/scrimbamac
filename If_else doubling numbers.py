4768e8c1a69d9cf080ccba5ffe935837877ca29a
sushantkadam <sushant.kadam27@gmail.com>
2026-08-24 15:03:48 +0530

    if else and Doubling

---
+++ If_else doubling numbers.py
@@ -0,0 +1,20 @@
+
+# If_else Logic
+greeting = "Hello"
+
+if greeting == "Hello":
+    print('Hello there !')
+
+else:
+    print("Greetings !")
+
+
+
+# For loop : Doubling Numbers
+
+numbers = [1,4, 7, 10]
+
+for n in numbers:
+    print(n * 3)
+
+
